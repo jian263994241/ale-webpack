21dc4311c2a8016568359041a89dbe449e25bba8
jianl <jianjianfengyun@126.com>
2019-03-11 15:53:29 +0800

    .

--- loaders.js
+++ loaders.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const preset = require('babel-preset');
 
+const LessPluginNpmImportPlugin = require('less-plugin-npm-import');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
+const lessPluginGlob = require('less-plugin-glob');
+const autoprefixer = new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
+const npmImport = new LessPluginNpmImportPlugin({ prefix: '~' });
+
 module.exports = function(options){
 
   const list = [
@@ -9,19 +15,56 @@ module.exports = function(options){
       exclude: /(node_modules|bower_components)/,
       include: options.context,
       enforce: 'pre',
-      use: {
-        loader: require.resolve('babel-loader'),
-        options: { presets: [preset] }
-      }
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: { presets: [ preset ] }
+        },
+        {
+          loader: require.resolve('string-replace-loader'),
+          options: {
+            multiple: [
+              { search: '__uri', replace: 'require', flags: 'g' },
+            ]
+          }
+        }
+      ]
     },
     {
-      test: /\.css$/,
+      test: /\.(less|css)$/,
       use: [
-       { loader: require.resolve('style-loader') },
-       { loader: require.resolve('css-loader') }
+        {
+          loader: require.resolve('style-loader')
+        },
+        {
+          loader: require.resolve('css-loader')
+        },
+        {
+          loader: require.resolve('less-loader'),
+          options: {
+            sourceMap: {
+              outputSourceFiles: true,
+              sourceMapFileInline: true,
+            },
+            javascriptEnabled: true,
+            plugins: [ npmImport, lessPluginGlob, autoprefixer ]
+          }
+        }
       ]
+    },
+    {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        require.resolve('file-loader'),
+        {
+          loader: require.resolve('image-webpack-loader'),
+          options: {
+            disable: true, // webpack@2.x and newer
+          },
+        },
+      ],
     }
   ];
-  
+
   return list;
 }
