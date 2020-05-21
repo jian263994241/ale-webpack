a8e674a5fe204c203f13aa85cc3a3b98ffab282a
jianl <jianjianfengyun@126.com>
2020-05-21 11:16:52 +0800

    fix: add setEnv default string

--- packages/ale-cli/utils/setEnv.js
+++ packages/ale-cli/utils/setEnv.js
@@ -1,4 +1,4 @@
-module.exports = function setEnv(env) {
+module.exports = function setEnv(env = '') {
   const args = env.split('=');
   if (args.length === 2) {
     process.env[args[0]] = args[1];
